b4c2bbf9f4594f73fd797fbf0909850979255afc
ElCabrino <cabrini.vincent@gmail.com>
2018-10-02 17:07:14 +0200

    Add a working registration status activity

--- app/src/main/java/ch/epfl/sweng/vanjel/RegistrationStatusActivity.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/RegistrationStatusActivity.java
@@ -1,20 +1,29 @@
 package ch.epfl.sweng.vanjel;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class RegistrationStatusActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //recuperating the registration status
-        Bundle b = getIntent().getExtras();
-        TextView text = findViewById(R.id.loginStatus);
-        //setting the text with the current registration status
-        text.setText(b.getString("status"));
-        //displaying
         setContentView(R.layout.activity_registration_status);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
