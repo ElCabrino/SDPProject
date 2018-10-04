7325313de64345b971e9c9297c1b3b23a6237585
ecaquot <etienne.caquot@epfl.ch>
2018-10-04 18:47:30 +0200

    Add test for chooseRegistration

---
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/ChooseRegistrationTest.java
@@ -0,0 +1,32 @@
+package ch.epfl.sweng.vanjel;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+
+@RunWith(AndroidJUnit4.class)
+public class ChooseRegistrationTest {
+    @Rule
+    public final IntentsTestRule<ChooseRegistration> iActivityRule =
+            new IntentsTestRule<>(ChooseRegistration.class);
+    @Test
+    public void testCanOpenPatientRegistration() {
+        onView(withId(R.id.patientButton)).perform(click());
+        intended(hasComponent(PatientRegistration.class.getName()));
+    }
+
+    @Test
+    public void testCanOpenDoctorRegistration() {
+        onView(withId(R.id.doctorButton)).perform(click());
+        intended(hasComponent(DoctorRegistration.class.getName()));
+    }
+}
