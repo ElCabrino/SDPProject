99997d5da81c034aae66f833c24b5f3bf896cd91
ElCabrino <cabrini.vincent@gmail.com>
2018-10-04 17:14:33 +0200

    Modify the enum class to be more easier to use

--- app/src/main/java/ch/epfl/sweng/vanjel/DoctorActivity.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/DoctorActivity.java
@@ -1,20 +1,11 @@
 package ch.epfl.sweng.vanjel;
 
 public enum DoctorActivity {
-    ENUM1("Generalist"),
-    ENUM2("Dentist"),
-    ENUM3("Physiotherapist"),
-    ENUM4("Psychologist"),
-    ENUM5("Orthopedist"),
-    ENUM6("Ophthalmologist");
+    Generalist,
+    Dentist,
+    Physiotherapist,
+    Psychologist,
+    Orthopedist,
+    Ophthalmologist;
 
-    private String friendlyName;
-
-    DoctorActivity(String friendlyName){
-        this.friendlyName = friendlyName;
-    }
-
-    @Override public String toString(){
-        return friendlyName;
-    }
 }
