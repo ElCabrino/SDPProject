6a9bfa9f0d53f83f5f3a0670af05c69374479707
ElCabrino <cabrini.vincent@gmail.com>
2018-10-04 17:13:45 +0200

    Add activity field

--- app/src/main/java/ch/epfl/sweng/vanjel/Doctor.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/Doctor.java
@@ -1,7 +1,11 @@
 package ch.epfl.sweng.vanjel;
 
 public class Doctor extends User {
-    Doctor(String email, String firstName, String lastName, String birthday, String street, String streetNumber, String city, String country, Gender gender) {
+
+    DoctorActivity activity;
+
+    Doctor(String email, String firstName, String lastName, String birthday, String street, String streetNumber, String city, String country, Gender gender, DoctorActivity activity) {
         super(email, firstName, lastName, birthday, street, streetNumber, city, country, gender);
+        this.activity = activity;
     }
 }
