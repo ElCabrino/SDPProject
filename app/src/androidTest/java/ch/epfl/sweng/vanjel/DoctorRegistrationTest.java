62eb33c324c20997014c8ac42883111aaade3919
ElCabrino <cabrini.vincent@gmail.com>
2018-10-05 10:50:47 +0200

    Add empty method to fix build

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/DoctorRegistrationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/DoctorRegistrationTest.java
@@ -3,8 +3,14 @@ package ch.epfl.sweng.vanjel;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class DoctorRegistrationTest {
+
+    @Test
+    public void testCanGreetUsers() {
+
+    }
 }
