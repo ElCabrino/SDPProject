1b65f6fb259d36b97673b16119c4d1e21ceacc64
Aslam CADER <aslam.cader@protonmail.com>
2018-10-07 23:19:57 +0200

    Add test Registration form with valid two passwords

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/PatientRegistrationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/PatientRegistrationTest.java
@@ -2,7 +2,9 @@ package ch.epfl.sweng.vanjel;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.DatePicker;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,9 +14,15 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
+
+import android.support.test.espresso.contrib.PickerActions;
+
 
 /**
  Test class for Patient Registration form
@@ -43,6 +51,57 @@ public class PatientRegistrationTest {
 
         String email = "test@test.ch";
         String password = "123456";
+        String confirmedPassword = "12345"; // not the same
+        String firstName = "John";
+        String lastName = "Smith";
+        String street = "Best avenue";
+        String streetNumber = "42";
+        String city = "Gaillard";
+        String country = "EPFL Land";
+        String postCode = "1212";
+
+//        int month = 10;
+//        int year = 1997;
+//        int day = 9;
+
+        String birthday = "09/10/1997";
+
+        onView(withId(R.id.mailReg)).perform(scrollTo(), typeText(email)).perform(closeSoftKeyboard());
+        onView(withId(R.id.passwordReg)).perform(scrollTo(), typeText(password)).perform(closeSoftKeyboard());
+        onView(withId(R.id.confirmPasswordReg)).perform(scrollTo(), typeText(confirmedPassword)).perform(closeSoftKeyboard());
+        onView(withId(R.id.firstNameReg)).perform(scrollTo(), typeText(firstName)).perform(closeSoftKeyboard());
+        onView(withId(R.id.lastNameReg)).perform(scrollTo(), typeText(lastName)).perform(closeSoftKeyboard());
+        onView(withId(R.id.birthdayReg)).perform(scrollTo(), typeText(birthday)).perform(closeSoftKeyboard());
+//        onView(withId(R.id.birthdayReg)).perform(scrollTo(), click());
+//        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(year, month , day));
+//        onView(withId(android.R.id.button1)).perform(click());
+//        onView(withId(R.id.birthdayReg)).check(matches(withText(day + "/" + month + "/" + year)));
+        onView(withId(R.id.streetReg)).perform(scrollTo(), typeText(street)).perform(closeSoftKeyboard());
+        onView(withId(R.id.numberReg)).perform(scrollTo(), typeText(streetNumber)).perform(closeSoftKeyboard());
+        onView(withId(R.id.cityReg)).perform(scrollTo(), typeText(city)).perform(closeSoftKeyboard());
+        onView(withId(R.id.countryReg)).perform(scrollTo(), typeText(country)).perform(closeSoftKeyboard());
+        onView(withId(R.id.postcodeReg)).perform(scrollTo(), typeText(postCode)).perform(closeSoftKeyboard());
+//
+
+        onView(withId(R.id.buttonReg)).perform(scrollTo(), click());
+
+
+        assertEquals("Unexpected Activity after putting data", mActivityRule.getActivity().getClass().getName(), Registration.class.getName());
+
+        // same password
+        onView(withId(R.id.confirmPasswordReg)).perform(scrollTo(), typeText("6")).perform(closeSoftKeyboard());
+        onView(withId(R.id.buttonReg)).perform(scrollTo(), click());
+
+//        assertEquals("Unexpected Activity after putting data", mActivityRule.getActivity().getClass().getName(), Registration.class.getName());
+
+
+    }
+
+    @Test
+    public void notValidFormTest(){
+
+        String email = "test.ch";
+        String password = "123456";
         String confirmedPassword = "123455"; // not the same
         String firstName = "John";
         String lastName = "Smith";
@@ -53,24 +112,8 @@ public class PatientRegistrationTest {
         String postCode = "1212";
 
         onView(withId(R.id.mailReg)).perform(scrollTo(), typeText(email)).perform(closeSoftKeyboard());
-        onView(withId(R.id.passwordReg)).perform(scrollTo(), typeText(password)).perform(closeSoftKeyboard());
-        onView(withId(R.id.confirmPasswordReg)).perform(scrollTo(), typeText(confirmedPassword)).perform(closeSoftKeyboard());
-        onView(withId(R.id.firstNameReg)).perform(scrollTo(), typeText(firstName)).perform(closeSoftKeyboard());
-        onView(withId(R.id.lastNameReg)).perform(scrollTo(), typeText(lastName)).perform(closeSoftKeyboard());
-//        OnView(withId(R.id.birthdayReg)).perform(scrollTo(), typeText(birthday)).perform(closeSoftKeyboard());
-        onView(withId(R.id.streetReg)).perform(scrollTo(), typeText(street)).perform(closeSoftKeyboard());
-        onView(withId(R.id.numberReg)).perform(scrollTo(), typeText(streetNumber)).perform(closeSoftKeyboard());
-        onView(withId(R.id.cityReg)).perform(scrollTo(), typeText(city)).perform(closeSoftKeyboard());
-        onView(withId(R.id.countryReg)).perform(scrollTo(), typeText(country)).perform(closeSoftKeyboard());
-        onView(withId(R.id.postcodeReg)).perform(scrollTo(), typeText(postCode)).perform(closeSoftKeyboard());
-
 
         onView(withId(R.id.buttonReg)).perform(scrollTo(), click());
-
-
-        assertEquals("Unexpected Activity after putting data", mActivityRule.getActivity().getClass().getName(), Registration.class.getName());
-        assertEquals("Unexpected Activity after putting data", mActivityRule.getActivity().getClass().getName(), Registration.class.getName());
-
 
     }
 
