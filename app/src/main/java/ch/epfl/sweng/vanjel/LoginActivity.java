9a83694318d3de89abeb29768033ac0badd3d866
ElCabrino <cabrini.vincent@gmail.com>
2018-10-08 11:58:35 +0200

    Implement skeleton for the login

--- app/src/main/java/ch/epfl/sweng/vanjel/LoginActivity.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/LoginActivity.java
@@ -5,27 +5,62 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    private TextView registrationLogin ;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+
+    private EditText emailField;
+    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        registrationLogin = findViewById(R.id.registationLogin);
+        //Views
+        emailField = findViewById(R.id.mailLogin);
+        passwordField = findViewById(R.id.passwordLogin);
 
-        registrationLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,ChooseRegistration.class);
-                startActivity(intent);
-            }
-        });
+        //Button listener
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+        findViewById(R.id.registationLogin).setOnClickListener(this);
+
+        //Initialize Auth
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        /* TODO: updater l'interface si deja loggé; */
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.buttonLogin) {
+            createAccount(emailField.getText().toString(), passwordField.getText().toString());
+        } else if (i == R.id.buttonReg){
+            Intent intent = new Intent(LoginActivity.this,ChooseRegistration.class);
+            startActivity(intent);
+        }
+    }
+
+    private void createAccount(String s, String s1) {
+    }
 
 }
