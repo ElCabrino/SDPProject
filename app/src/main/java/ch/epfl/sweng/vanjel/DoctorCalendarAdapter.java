0ce2462d3062ac02e0b24df9dad36e19076d61c6
ecaquot <etienne.caquot@epfl.ch>
2018-10-10 22:05:09 +0200

    Fix for travis

--- app/src/main/java/ch/epfl/sweng/vanjel/DoctorCalendarAdapter.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/DoctorCalendarAdapter.java
@@ -3,23 +3,13 @@ package ch.epfl.sweng.vanjel;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
-import static android.support.constraint.Constraints.TAG;
 
 public class DoctorCalendarAdapter extends RecyclerView.Adapter<DoctorCalendarAdapter.ViewHolder> {
 
@@ -47,7 +37,7 @@ public class DoctorCalendarAdapter extends RecyclerView.Adapter<DoctorCalendarAd
         holder.patient.setText(mData.get(position).getPatient().toString());
         holder.delete.setOnClickListener(new View.OnClickListener() {
                                              @Override
-                                             public void onClick(View v) {delete(position);
+                                             public void onClick(View v) {delete(holder.getAdapterPosition());
                                              }
                                          }
         );
