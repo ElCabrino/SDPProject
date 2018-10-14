a7ea8825e898cb9842152c98ccc810aede798f64
ElCabrino <cabrini.vincent@gmail.com>
2018-10-14 14:04:11 +0200

    Remove all warnings

--- app/src/main/java/ch/epfl/sweng/vanjel/PatientInfo.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/PatientInfo.java
@@ -1,25 +1,14 @@
 package ch.epfl.sweng.vanjel;
 
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,16 +43,6 @@ public class PatientInfo extends AppCompatActivity implements View.OnClickListen
     Button buttonDrinking;
     Button buttonExercise;
 
-    DatabaseReference databaseCondition;
-    DatabaseReference databaseSurgery;
-    DatabaseReference databaseAllergy;
-    DatabaseReference databaseDrugReaction;
-    DatabaseReference databaseDrug;
-    DatabaseReference databaseSubstance;
-    DatabaseReference databaseSmoking;
-    DatabaseReference databaseDrinking;
-    DatabaseReference databaseExercise;
-
     ListView listViewConditions;
     ListView listViewSurgeries;
     ListView listViewAllergies;
@@ -90,9 +69,7 @@ public class PatientInfo extends AppCompatActivity implements View.OnClickListen
         patientInfoDatabaseService =
                 new PatientInfoDatabaseService(FirebaseAuth.getInstance().getUid(), this);
 
-        saveButton = (Button) findViewById(R.id.buttonGenInfoPtReg);
-
-        getFirebaseInstances();
+        saveButton = findViewById(R.id.buttonGenInfoPtReg);
 
         getAllEditText();
 
@@ -126,28 +103,16 @@ public class PatientInfo extends AppCompatActivity implements View.OnClickListen
         substanceList = new ArrayList<>();
     }
 
-    private void getFirebaseInstances() {
-        databaseCondition = FirebaseDatabase.getInstance().getReference("Patient").child("ABLlrLukjAaPzaf5GA03takkw5k2").child("Condition");
-        databaseSurgery = FirebaseDatabase.getInstance().getReference("Surgery");
-        databaseAllergy = FirebaseDatabase.getInstance().getReference("Allergy");
-        databaseDrugReaction = FirebaseDatabase.getInstance().getReference("DrugReaction");
-        databaseDrug = FirebaseDatabase.getInstance().getReference("Drug");
-        databaseSubstance = FirebaseDatabase.getInstance().getReference("Substance");
-        databaseSmoking = FirebaseDatabase.getInstance().getReference("Smoking");
-        databaseDrinking = FirebaseDatabase.getInstance().getReference("Drinking");
-        databaseExercise = FirebaseDatabase.getInstance().getReference("Exercise");
-    }
-
     private void getAllPatientInfoFields() {
-        listViewConditions = (ListView) findViewById(R.id.ptPriorConditionsList);
-        listViewSurgeries = (ListView) findViewById(R.id.ptSurgeryList);
-        listViewAllergies = (ListView) findViewById(R.id.ptAllergyList);
-        listViewDrugReactions = (ListView) findViewById(R.id.ptDrugReactionList);
-        listViewDrugs = (ListView) findViewById(R.id.ptDrugRegimenList);
-        listViewSubstances = (ListView) findViewById(R.id.ptSubstanceList);
-        textViewSmoking = (TextView) findViewById(R.id.ptSmokingValue);
-        textViewDrinking = (TextView) findViewById(R.id.ptDrinkingValue);
-        textViewExercise = (TextView) findViewById(R.id.ptExerciseValue);
+        listViewConditions = findViewById(R.id.ptPriorConditionsList);
+        listViewSurgeries = findViewById(R.id.ptSurgeryList);
+        listViewAllergies = findViewById(R.id.ptAllergyList);
+        listViewDrugReactions = findViewById(R.id.ptDrugReactionList);
+        listViewDrugs = findViewById(R.id.ptDrugRegimenList);
+        listViewSubstances = findViewById(R.id.ptSubstanceList);
+        textViewSmoking = findViewById(R.id.ptSmokingValue);
+        textViewDrinking = findViewById(R.id.ptDrinkingValue);
+        textViewExercise = findViewById(R.id.ptExerciseValue);
     }
 
     private void getAllButtons() {
@@ -163,19 +128,19 @@ public class PatientInfo extends AppCompatActivity implements View.OnClickListen
     }
 
     private void getAllEditText() {
-        priorConditionsReg = (EditText) findViewById(R.id.ptPriorConditionsReg);
-        surgeriesReg = (EditText) findViewById(R.id.ptSurgeryReg);
-        surgeriesYearReg = (EditText) findViewById(R.id.ptSurgeryYearReg);
-        allergyReg = (EditText) findViewById(R.id.ptAllergyReg);
-        drugReactionDrugReg = (EditText) findViewById(R.id.ptDrugReactionDrugReg);
-        drugReactionReactionReg = (EditText) findViewById(R.id.ptDrugReactionReactionReg);
-        drugRegimenDrugReg = (EditText) findViewById(R.id.ptDrugRegimenDrugReg);
-        drugRegimenDosageReg = (EditText) findViewById(R.id.ptDrugRegimenDosageReg);
-        drugRegimenTimesReg = (EditText) findViewById(R.id.ptDrugRegimenTimesReg);
-        substancesReg = (EditText) findViewById(R.id.ptSubstanceReg);
-        smokingReg = (EditText) findViewById(R.id.ptSmokingReg);
-        drinkingReg = (EditText) findViewById(R.id.ptDrinkingReg);
-        exerciseReg = (EditText) findViewById(R.id.ptExerciseReg);
+        priorConditionsReg = findViewById(R.id.ptPriorConditionsReg);
+        surgeriesReg = findViewById(R.id.ptSurgeryReg);
+        surgeriesYearReg = findViewById(R.id.ptSurgeryYearReg);
+        allergyReg = findViewById(R.id.ptAllergyReg);
+        drugReactionDrugReg = findViewById(R.id.ptDrugReactionDrugReg);
+        drugReactionReactionReg = findViewById(R.id.ptDrugReactionReactionReg);
+        drugRegimenDrugReg = findViewById(R.id.ptDrugRegimenDrugReg);
+        drugRegimenDosageReg = findViewById(R.id.ptDrugRegimenDosageReg);
+        drugRegimenTimesReg = findViewById(R.id.ptDrugRegimenTimesReg);
+        substancesReg = findViewById(R.id.ptSubstanceReg);
+        smokingReg = findViewById(R.id.ptSmokingReg);
+        drinkingReg = findViewById(R.id.ptDrinkingReg);
+        exerciseReg = findViewById(R.id.ptExerciseReg);
     }
 
 
@@ -192,120 +157,6 @@ public class PatientInfo extends AppCompatActivity implements View.OnClickListen
         patientInfoDatabaseService.addSmokingListener(textViewSmoking);
         patientInfoDatabaseService.addDrinkingListener(textViewDrinking);
         patientInfoDatabaseService.addExerciseListener(textViewExercise);
-    }
-
-    private void addAllergy() {
-        String allergy = allergyReg.getText().toString().trim();
-        if(!TextUtils.isEmpty(allergy)) {
-
-            String id = databaseAllergy.push().getKey();
-            Allergy al = new Allergy(id,allergy);
-
-
-            databaseAllergy.child(id).setValue(al);
-            Toast.makeText(this,"Allergy added",Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this,"Please enter the allergy you want to add", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void addDrugReaction() {
-        String drug = drugReactionDrugReg.getText().toString().trim();
-        String reaction = drugReactionReactionReg.getText().toString().trim();
-        if(!TextUtils.isEmpty(drug) && !TextUtils.isEmpty(reaction)) {
-
-            String id = databaseDrugReaction.push().getKey();
-            DrugReaction dr = new DrugReaction(id,drug,reaction);
-
-            databaseDrugReaction.child(id).setValue(dr);
-            Toast.makeText(this,"Drug reaction added",Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this,"Please enter the drug and the reaction you want to add", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void addDrug() {
-        String drug = drugRegimenDrugReg.getText().toString().trim();
-        String dosage = drugRegimenDosageReg.getText().toString().trim();
-        String times = drugRegimenTimesReg.getText().toString().trim();
-
-        if(!TextUtils.isEmpty(drug) && !TextUtils.isEmpty(dosage) && !TextUtils.isEmpty(times)) {
-
-            String id = databaseDrug.push().getKey();
-            Drug dr = new Drug(id,drug,dosage, times);
-
-            databaseDrug.child(id).setValue(dr);
-            Toast.makeText(this,"Drug added",Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this,"Please enter the drug, the dosage and the frequency you want to add", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void addSubstance() {
-        String substance = substancesReg.getText().toString().trim();
-
-        if(!TextUtils.isEmpty(substance)) {
-
-            String id = databaseSubstance.push().getKey();
-            Substance sub = new Substance(id,substance);
-
-            databaseSubstance.child(id).setValue(sub);
-            Toast.makeText(this,"Substance added",Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this,"Please enter the substance you want to add", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void addSmoking() {
-        String amount = smokingReg.getText().toString().trim();
-
-        if(!TextUtils.isEmpty(amount)) {
-
-            String id = databaseSmoking.push().getKey();
-            Smoking cigs = new Smoking(id,amount);
-
-            databaseSmoking.child(id).setValue(cigs);
-            Toast.makeText(this,"Smoking added",Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this,"Please enter the average amount of cigarettes you smoke per day", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void addDrinking() {
-        String amount = drinkingReg.getText().toString().trim();
-
-        if(!TextUtils.isEmpty(amount)) {
-
-            String id = databaseDrinking.push().getKey();
-            Drinking drink = new Drinking(id,amount);
-
-            databaseDrinking.child(id).setValue(drink);
-            Toast.makeText(this,"Drinking added",Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this,"Please enter the average amount of alcohol you consume per day", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void addExercise() {
-        String amount = exerciseReg.getText().toString().trim();
-
-        if(!TextUtils.isEmpty(amount)) {
-
-            String id = databaseExercise.push().getKey();
-            Exercise ex = new Exercise(id,amount);
-
-            databaseExercise.child(id).setValue(ex);
-            Toast.makeText(this,"Exercise added",Toast.LENGTH_LONG).show();
-
-        } else {
-            Toast.makeText(this,"Please enter the average hours of exercise you do per week", Toast.LENGTH_LONG).show();
-        }
     }
 
     @Override
