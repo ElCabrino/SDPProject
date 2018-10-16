d41c5d0c188dba7d4e526ba26010b00e6bede935
Aslam CADER <aslam.cader@protonmail.com>
2018-10-17 00:08:11 +0200

    Fix Travis Build

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/FilteredDoctorsTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/FilteredDoctorsTest.java
@@ -3,11 +3,16 @@ package ch.epfl.sweng.vanjel;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
+
+import static android.support.test.espresso.Espresso.onView;
 /**
  * @author Aslam CADER
  * @reviewer
@@ -17,8 +22,13 @@ import static org.junit.Assert.*;
 public class FilteredDoctorsTest {
 
     @Rule
-    public ActivityTestRule<FilteredDoctors> mActivityRule = new ActivityTestRule<>(
-            FilteredDoctors.class);
+    public ActivityTestRule<SearchDoctor> mActivityRule = new ActivityTestRule<>(
+            SearchDoctor.class);
+
+    @Before
+    public void setUp(){
+        onView(withId(R.id.buttonSearch)).perform(click());
+    }
 
 
     // check that we are in the correct activity
