a9afabb56950bd0d7dca2537f4086cba061c7765
Luca Joss <luca.joss@hispeed.ch>
2018-10-16 20:58:33 +0200

    Added SearchDoctor test

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/SearchDoctorTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/SearchDoctorTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.sweng.vanjel;
 
+import android.os.Bundle;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -8,11 +9,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchDoctorTest {
@@ -34,6 +40,22 @@ public class SearchDoctorTest {
         onView(withId(R.id.lastNameSearch)).perform(scrollTo()).check(matches(withText(lastName)));
         onView(withId(R.id.specialisationSearch)).perform(scrollTo()).check(matches(withText(specialisation)));
         onView(withId(R.id.citySearch)).perform(scrollTo()).check(matches(withText(city)));
+    }
+
+    @Test
+    public void searchDoctorTest() {
+        Bundle b = new Bundle();
+        b.putString("lastname", lastName);
+        b.putString("name", firstName);
+        b.putString("specialisation", specialisation);
+        b.putString("city", city);
+
+        fillFields();
+        onView(withId(R.id.buttonSearch)).perform(scrollTo(), click());
+        intended(hasExtra("lastname", lastName));
+        intended(hasExtra("name", firstName));
+        intended(hasExtra("specialisation", specialisation));
+        intended(hasExtra("city", city));
     }
 
     private void enterFirstName(String firstName) {
