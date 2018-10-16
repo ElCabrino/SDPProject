f94533a9c96f27233e81e22c401d2b86127024cc
Luca Joss <luca.joss@hispeed.ch>
2018-10-16 18:07:59 +0200

    Fixed a test in SearchDoctor and added ScrollView for small screens.

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/SearchDoctorTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/SearchDoctorTest.java
@@ -3,40 +3,37 @@ package ch.epfl.sweng.vanjel;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchDoctorTest {
 
+    String firstName = "Luca";
+    String lastName = "Joss";
+    String specialisation = "ORL";
+    String city = "Morges";
 
     @Rule
     public final IntentsTestRule<SearchDoctor> ActivityRule =
             new IntentsTestRule<>(SearchDoctor.class);
 
-    @Before
-    public void setUp() {
-
-    }
-
     @Test
     public void getFieldStringsTest() {
-        enterFirstName("Luca");
-        enterLastName("Joss");
-        enterSpecialisation("ORL");
-        enterCity("Morges");
+        fillFields();
 
-//        assertEquals("Luca", withId(R.id.firstNameSearch).toString().trim());
-//        assertEquals("Joss", withId(R.id.lastNameSearch).toString().trim());
-//        assertEquals("ORL", withId(R.id.specialisationSearch).toString().trim());
-//        assertEquals("Morges", withId(R.id.citySearch).toString().trim());
+        onView(withId(R.id.firstNameSearch)).perform(scrollTo()).check(matches(withText(firstName)));
+        onView(withId(R.id.lastNameSearch)).perform(scrollTo()).check(matches(withText(lastName)));
+        onView(withId(R.id.specialisationSearch)).perform(scrollTo()).check(matches(withText(specialisation)));
+        onView(withId(R.id.citySearch)).perform(scrollTo()).check(matches(withText(city)));
     }
 
     private void enterFirstName(String firstName) {
@@ -53,5 +50,12 @@ public class SearchDoctorTest {
 
     private void enterCity(String city) {
         onView(withId(R.id.citySearch)).perform(replaceText(city));
+    }
+
+    private void fillFields() {
+        enterFirstName(firstName);
+        enterLastName(lastName);
+        enterSpecialisation(specialisation);
+        enterCity(city);
     }
 }
