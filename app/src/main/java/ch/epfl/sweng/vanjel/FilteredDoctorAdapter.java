63580b872bba394c30acda55e482490a48cb29f2
Aslam CADER <aslam.cader@protonmail.com>
2018-10-22 16:34:13 +0200

    Change doctors from array to HashMap with key value

--- app/src/main/java/ch/epfl/sweng/vanjel/FilteredDoctorAdapter.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/FilteredDoctorAdapter.java
@@ -3,6 +3,7 @@ package ch.epfl.sweng.vanjel;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,25 +11,31 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static ch.epfl.sweng.vanjel.R.layout.layout_doctor_cardview;
 
 public class FilteredDoctorAdapter extends recyclerViewAdapter<FilteredDoctorAdapter.ViewHolder> {
 
     ArrayList<Doctor> doctors;
+    HashMap<String, Doctor> doctorHashMap;
     Context context;
 
 
-    public FilteredDoctorAdapter(Context context, ArrayList<Doctor> data){
-        this.doctors = data;
+    public FilteredDoctorAdapter(Context context, HashMap<String, Doctor> data){
+        this.doctorHashMap = data;
         this.context = context;
+        this.doctors = new ArrayList<>();
+        // loop for to take doctorHashmap to doctor
+        for(Doctor doc: doctorHashMap.values())
+            doctors.add(doc);
     }
 
 
     @NonNull
     @Override
     public FilteredDoctorAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-       return new FilteredDoctorAdapter.ViewHolder(LayoutInflater.from(context).inflate(R.layout.layout_doctor_cardview, viewGroup, false));
+        return new FilteredDoctorAdapter.ViewHolder(LayoutInflater.from(context).inflate(R.layout.layout_doctor_cardview, viewGroup, false));
 //        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_doctor_cardview,
 //                viewGroup,false);
 //        ViewHolder holder = new ViewHolder(view);
@@ -38,6 +45,7 @@ public class FilteredDoctorAdapter extends recyclerViewAdapter<FilteredDoctorAda
 
     @Override
     public void onBindViewHolder(@NonNull FilteredDoctorAdapter.ViewHolder viewHolder, int i) {
+
         viewHolder.firstName.setText(doctors.get(i).getFirstName());
         viewHolder.lastName.setText(doctors.get(i).getLastName());
         viewHolder.activity.setText(doctors.get(i).getActivity());
