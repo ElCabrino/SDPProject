d3d368950fa2e40026140332e390b5457e898ac6
Luca Joss <luca.joss@hispeed.ch>
2018-10-30 14:07:46 +0100

    Add content description to find view in test.

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/DoctorInformationTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/DoctorInformationTest.java
@@ -21,6 +21,7 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -45,7 +46,7 @@ public class DoctorInformationTest {
 
         intended(hasComponent(FilteredDoctors.class.getName()));
 
-        onView(withId(R.id.docInfo_lastName)).check(matches(withText(lastName))).perform(click());
+        onView(withContentDescription("doctor information last name")).check(matches(withText(lastName))).perform(click());
 
 
         TimeUnit.SECONDS.sleep(5); // wait for data retrieving
