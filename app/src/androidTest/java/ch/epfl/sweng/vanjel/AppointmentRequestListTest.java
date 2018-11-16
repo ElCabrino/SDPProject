301a02af38fbb927163b306289408d173aa9e195
Vinc <cabrini.vincent@gmail.com>
2018-11-16 12:14:09 +0100

    Fix tests #2

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/AppointmentRequestListTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/AppointmentRequestListTest.java
@@ -25,8 +25,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class AppointmentRequestListTest {
 
-    @BeforeClass
-    public static void init() {
+    @Before
+    public void init() {
         FirebaseAuthCustomBackend.setNullUser(false);
         FirebaseAuthCustomBackend.setMockPatient(false);
     }
