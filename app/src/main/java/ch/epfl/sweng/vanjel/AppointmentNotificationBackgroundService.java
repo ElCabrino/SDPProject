a7843417665665f4128fb3b0dd7073fa82c41524
Vinc <cabrini.vincent@gmail.com>
2018-11-17 19:12:49 +0100

    Make doctor notification functionnal

--- app/src/main/java/ch/epfl/sweng/vanjel/AppointmentNotificationBackgroundService.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/AppointmentNotificationBackgroundService.java
@@ -59,12 +59,10 @@ public class AppointmentNotificationBackgroundService extends Service {
         ref.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-                Log.d("LISTENERTEST", dataSnapshot.toString());
-                Log.d("LISTENERTEST", dataSnapshot.child("doctor").toString());
                 String doctor = dataSnapshot.child("doctor").getValue().toString();
                 Boolean notify = Boolean.parseBoolean(dataSnapshot.child("doctorNotified").getValue().toString());
                 if (!notify) {
-                    dataSnapshot.getRef().child("doctorNotified").setValue("true");
+                    dataSnapshot.getRef().child("doctorNotified").setValue(true);
                     notifyDoctor(doctor);
                 }
             }
