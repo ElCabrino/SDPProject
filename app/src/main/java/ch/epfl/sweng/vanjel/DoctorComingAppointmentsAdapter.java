eb866fba1db7045b58ee1a1bd8b0ebf6bc868d0f
Aslam CADER <aslam.cader@protonmail.com>
2018-11-21 19:45:19 +0100

    Display today if doctorNextAppointment is today

--- app/src/main/java/ch/epfl/sweng/vanjel/DoctorComingAppointmentsAdapter.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/DoctorComingAppointmentsAdapter.java
@@ -9,17 +9,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DoctorComingAppointmentsAdapter extends recyclerViewAdapter<DoctorComingAppointmentsAdapter.ViewHolder> {
 
     ArrayList<Appointment> appointments;
+    private SimpleDateFormat formatter = new SimpleDateFormat("E MMM dd yyyy");
+    Date currentDate;
     Context context;
     public DoctorComingAppointmentsAdapter(Context context, ArrayList<Appointment> givenAppointments){
         this.context = context;
         this.appointments = givenAppointments;
+        this.currentDate = new Date();
     }
 
     @NonNull
@@ -31,11 +37,34 @@ public class DoctorComingAppointmentsAdapter extends recyclerViewAdapter<DoctorC
     @Override
     public void onBindViewHolder(@NonNull DoctorComingAppointmentsAdapter.ViewHolder viewHolder, int i) {
         viewHolder.lastName.setText(appointments.get(i).getDuration().toString());
-        viewHolder.date.setText(appointments.get(i).getDay());
         viewHolder.time.setText(appointments.get(i).getHour());
+
+        // if today, "Today"
+        Boolean isToday;
+        try {
+            isToday = isAppointmentToday(appointments.get(i));
+        } catch (ParseException e) {
+            isToday = false;
+            e.printStackTrace();
+        }
+
+        if(isToday){
+            viewHolder.date.setText("Today");
+        } else {
+            viewHolder.date.setText(appointments.get(i).getDay());
+        }
+
+
     }
 
 
+    public boolean isAppointmentToday(Appointment appointment) throws ParseException {
+        currentDate = formatter.parse(formatter.format(currentDate));
+        int comparator = formatter.parse(appointment.getDay()).compareTo(currentDate);
+
+        if(comparator == 0) return true;
+        else return false;
+    }
 
     @Override
     public int getItemCount() { return appointments.size();  }
