c390c306f302778bda7f2f2656400cbfa0ec808b
ecaquot <etienne.caquot@epfl.ch>
2018-12-08 22:04:50 +0100

    Create a new main layout after when logged in

---
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/MainMenuTest.java
@@ -0,0 +1,133 @@
+package ch.epfl.sweng.vanjel;
+
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import ch.epfl.sweng.vanjel.chat.ChatListActivity;
+import ch.epfl.sweng.vanjel.favorite.LocalDatabaseService;
+import ch.epfl.sweng.vanjel.favoriteList.PatientFavoriteListActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static ch.epfl.sweng.vanjel.TestHelper.restoreMockFlags;
+import static ch.epfl.sweng.vanjel.TestHelper.setupNoExtras;
+import static org.junit.Assert.assertEquals;
+
+public class MainMenuTest {
+
+    @Rule
+    public ActivityTestRule<MainMenu> mActivityRule =
+            new ActivityTestRule<>(MainMenu.class, true, false);
+
+    @BeforeClass
+    public static void setUp() {
+        Intents.init();
+    }
+
+    @AfterClass
+    public static void restoreIntents() {
+        Intents.release();
+        restoreMockFlags();
+    }
+
+    @Test
+    public void searchDoctorButtonTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, true, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.searchMainMenu)).perform(click());
+        intended(hasComponent(SearchDoctor.class.getName()));
+    }
+
+    @Test
+    public void patientInfoButtonAsPatientTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, true, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.infosMainMenu)).perform(click());
+        intended(hasComponent(PatientInfo.class.getName()));
+    }
+
+    @Test
+    public void setAvailabilityButton() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, false, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.setAvailMainMenu)).perform(click());
+        intended(hasComponent(DoctorAvailabilityActivity.class.getName()));
+    }
+
+    @Test
+    public void nearbyDoctorButtonTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, true, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.nearbyMainMenu)).perform(click());
+        intended(hasComponent(NearbyDoctor.class.getName()));
+    }
+
+    @Test
+    public void treatedPatientsButtonTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, false, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.treatedMainMenu)).perform(click());
+        intended(hasComponent(TreatedPatients.class.getName()));
+    }
+
+    @Test
+    public void favoriteListButtonTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, true, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.favoriteMainMenu)).perform(click());
+        intended(hasComponent(PatientFavoriteListActivity.class.getName()));
+    }
+
+    @Test
+    public void nextAppointmentsAsPatientTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, true, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.nextAppointMainMenu)).perform(click());
+        intended(hasComponent(PatientPersonalAppointments.class.getName()));
+    }
+
+    @Test
+    public void nextAppointmentsAsDoctorTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, false, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.nextAppointMainMenu)).perform(click());
+        intended(hasComponent(DoctorComingAppointments.class.getName()));
+
+    }
+
+    @Test
+    public void requestsListTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, false, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.requestsMainMenu)).perform(click());
+        intended(hasComponent(DoctorAppointmentsList.class.getName()));
+    }
+
+    @Test
+    public void chatAccessTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, true, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.chatMainMenu)).perform(click());
+        intended(hasComponent(ChatListActivity.class.getName()));
+    }
+
+    @Test
+    public void logOutTest() throws Exception {
+        setupNoExtras(MainMenu.class, mActivityRule, false, true, false, false, false, false);
+        TimeUnit.SECONDS.sleep(1);
+        onView(withId(R.id.logoutMainMenu)).perform(click());
+        //verifiy if local favorite are erased
+        LocalDatabaseService l = new LocalDatabaseService(mActivityRule.getActivity().getApplicationContext());
+        assertEquals(0, l.getAll().size());
+    }
+}
---
+++ app/src/main/java/ch/epfl/sweng/vanjel/MainMenu.java
@@ -0,0 +1,163 @@
+package ch.epfl.sweng.vanjel;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import ch.epfl.sweng.vanjel.chat.ChatListActivity;
+import ch.epfl.sweng.vanjel.favorite.LocalDatabaseService;
+import ch.epfl.sweng.vanjel.favoriteList.PatientFavoriteListActivity;
+import ch.epfl.sweng.vanjel.forwardRequest.ForwardRequest;
+
+/**
+ * @author Etienne CAQUOT
+ */
+public class MainMenu extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView logout, profile, nextAppointement, chat, requests, search, treated, nearby,
+            setAvailability, favorite, infos, recommendations;
+
+    private String userType;
+
+    private final FirebaseDatabase database = FirebaseDatabaseCustomBackend.getInstance();
+    private final FirebaseAuth auth = FirebaseAuthCustomBackend.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        init();
+        isUserPatient();
+    }
+
+    private void init() {
+        logout = findViewById(R.id.logoutMainMenu);
+        logout.setOnClickListener(this);
+        profile = findViewById(R.id.profileMainMenu);
+        profile.setOnClickListener(this);
+        nextAppointement = findViewById(R.id.nextAppointMainMenu);
+        nextAppointement.setOnClickListener(this);
+        chat = findViewById(R.id.chatMainMenu);
+        chat.setOnClickListener(this);
+        requests = findViewById(R.id.requestsMainMenu);
+        requests.setOnClickListener(this);
+        search = findViewById(R.id.searchMainMenu);
+        search.setOnClickListener(this);
+        treated = findViewById(R.id.treatedMainMenu);
+        treated.setOnClickListener(this);
+        nearby = findViewById(R.id.nearbyMainMenu);
+        nearby.setOnClickListener(this);
+        setAvailability = findViewById(R.id.setAvailMainMenu);
+        setAvailability.setOnClickListener(this);
+        favorite = findViewById(R.id.favoriteMainMenu);
+        favorite.setOnClickListener(this);
+        infos = findViewById(R.id.infosMainMenu);
+        infos.setOnClickListener(this);
+        recommendations = findViewById(R.id.recommendationsMainMenu);
+        recommendations.setOnClickListener(this);
+    }
+
+    void isUserPatient() {
+        DatabaseReference patientRef = database.getReference("Patient");
+        patientRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(auth.getCurrentUser().getUid())) {
+                    userType = "Patient";
+                } else {
+                    userType = "Doctor";
+                }
+                setLayout();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("ERROR", "The read failed: " + databaseError.getCode());
+            }
+        });
+    }
+
+    private void setLayout() {
+        if (userType.equals("Patient")){
+            requests.setVisibility(View.INVISIBLE);
+            treated.setVisibility(View.INVISIBLE);
+            setAvailability.setVisibility(View.INVISIBLE);
+        } else {
+            search.setVisibility(View.INVISIBLE);
+            nearby.setVisibility(View.INVISIBLE);
+            favorite.setVisibility(View.INVISIBLE);
+            infos.setVisibility(View.INVISIBLE);
+            recommendations.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.logoutMainMenu:
+                logOut();
+                break;
+            case R.id.profileMainMenu:
+                startActivity(new Intent(this, Profile.class).putExtra("userType",userType));
+                break;
+            case R.id.nextAppointMainMenu:
+                nextAppointments();
+                break;
+            case R.id.chatMainMenu:
+                startActivity(new Intent(this, ChatListActivity.class));
+                break;
+            case R.id.requestsMainMenu:
+                startActivity(new Intent(this ,DoctorAppointmentsList.class));
+                break;
+            case R.id.searchMainMenu:
+                startActivity(new Intent(this, SearchDoctor.class).putExtra("isForward",false)
+                        .putExtra("doctor1Forward","").putExtra("patientForward",""));
+                break;
+            case R.id.treatedMainMenu:
+                startActivity(new Intent(this, TreatedPatients.class));
+                break;
+            case R.id.nearbyMainMenu:
+                startActivity(new Intent(this, NearbyDoctor.class));
+                break;
+            case R.id.setAvailMainMenu:
+                startActivity(new Intent(this, DoctorAvailabilityActivity.class));
+                break;
+            case R.id.favoriteMainMenu:
+                startActivity(new Intent(this, PatientFavoriteListActivity.class));
+                break;
+            case R.id.infosMainMenu:
+                startActivity(new Intent(this, PatientInfo.class));
+                break;
+            case R.id.recommendationsMainMenu:
+                startActivity(new Intent(this, ForwardRequest.class));
+                break;
+        }
+    }
+
+    private void logOut(){
+        LocalDatabaseService l = new LocalDatabaseService(this);
+        l.nuke();
+        auth.signOut();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+
+    private void nextAppointments(){
+        if (userType.equals("Patient")) {
+            startActivity(new Intent(this, PatientPersonalAppointments.class));
+        } else {
+            startActivity(new Intent(this, DoctorComingAppointments.class));
+        }
+    }
+}
