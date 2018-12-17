f85ce871c3d10dbe182d5bb58010aeebfa3f264b
AAA97AAA <aslam.cader@protonmail.com>
2018-12-17 09:42:59 +0100

    Remove top element in list and improve doctor layout

--- app/src/main/java/ch/epfl/sweng/vanjel/searchDoctor/FilteredDoctorAdapter.java
+++ app/src/main/java/ch/epfl/sweng/vanjel/searchDoctor/FilteredDoctorAdapter.java
@@ -165,7 +165,7 @@ public class FilteredDoctorAdapter extends RecyclerViewAdapter<FilteredDoctorAda
             country = itemView.findViewById(R.id.country);
             forwardButton = itemView.findViewById(R.id.forwardButtonInFilteredDoctors);
 
-            if(!isForward) forwardButton.setVisibility(View.INVISIBLE);
+            if(!isForward) forwardButton.setVisibility(View.GONE);
         }
     }
 }
