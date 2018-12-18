117c1cc492675daa9bf55abe5582fe24cde1fb7c
Luca Joss <luca.joss@hispeed.ch>
2018-12-18 17:01:54 +0100

    Fix test issues after merging master

--- app/src/androidTest/java/ch/epfl/sweng/vanjel/patientAppointment/PatientPersonalAppointmentsTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/vanjel/patientAppointment/PatientPersonalAppointmentsTest.java
@@ -18,6 +18,7 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sweng.vanjel.TestHelper.restoreMockFlags;
@@ -26,22 +27,22 @@ import static org.hamcrest.CoreMatchers.anything;
 
 public class PatientPersonalAppointmentsTest {
 
-    private final String ap1DateString = "Tue Jan 16 2018";
-    private final String ap2DateString = "Tue Feb 06 2018";
-    private final String ap3DateString = "Tue Mar 06 2018";
-    private final String ap4DateString = "Tue Apr 03 2018";
-    private final String ap5DateString = "Tue May 15 2018";
-    private final String ap6DateString = "Tue Jun 12 2018";
-    private final String ap7DateString = "Tue Jul 10 2018";
-    private final String ap8DateString = "Tue Aug 14 2018";
-    private final String ap9DateString = "Tue Sep 11 2018";
-    private final String ap10DateString = "Tue Oct 16 2018";
-    private final String ap11DateString = "Tue Nov 20 2018";
-    private final String ap12DateString = "Tue Dec 18 2018";
+    private final String ap1DateString = "Tue Jan 16 2019";
+    private final String ap2DateString = "Tue Feb 06 2019";
+    private final String ap3DateString = "Tue Mar 06 2019";
+    private final String ap4DateString = "Tue Apr 03 2019";
+    private final String ap5DateString = "Tue May 15 2019";
+    private final String ap6DateString = "Tue Jun 12 2019";
+    private final String ap7DateString = "Tue Jul 10 2019";
+    private final String ap8DateString = "Tue Aug 14 2019";
+    private final String ap9DateString = "Tue Sep 11 2019";
+    private final String ap10DateString = "Tue Oct 16 2019";
+    private final String ap11DateString = "Tue Nov 20 2019";
+    private final String ap12DateString = "Tue Dec 18 2019";
     private final String apTimeString = "10:00";
     private final String apTimeString2 = "14:00";
     private final String docNameString = "ln_dtest1";
-    private final String docAddressString = "11 street_dtest1 city_dtest1";
+    private final String docAddressString = "street_dtest1, 11 - city_dtest1";
 
     @Rule
     public final IntentsTestRule<PatientPersonalAppointments> ActivityRule =
@@ -169,13 +170,10 @@ public class PatientPersonalAppointmentsTest {
         FirebaseDatabaseCustomBackend.setDateFlag(1);
         setupNoExtras(PatientPersonalAppointments.class, ActivityRule, false, true, false, true, false, false, false);
         TimeUnit.SECONDS.sleep(1);
-        onData(anything()).inAdapterView(withId(R.id.ptPersonalAppointmentsListView)).atPosition(0).onChildView(withId(R.id.textViewAppointmentDoctorDate)).perform(scrollTo(),closeSoftKeyboard()).check(matches(withText(ap1DateString+" - Dr. (pending)")));
-        onData(anything()).inAdapterView(withId(R.id.ptPersonalAppointmentsListView)).atPosition(0).onChildView(withId(R.id.textViewAppointmentTime)).perform(scrollTo(),closeSoftKeyboard()).check(matches(withText(apTimeString+", 0 minutes")));
-        onData(anything()).inAdapterView(withId(R.id.ptPersonalAppointmentsListView)).atPosition(0).onChildView(withId(R.id.textViewAppointmentLocation)).perform(scrollTo(),closeSoftKeyboard()).check(matches(withText("")));
-
-        onData(anything()).inAdapterView(withId(R.id.ptPersonalAppointmentsListView)).atPosition(1).onChildView(withId(R.id.textViewAppointmentDoctorDate)).perform(scrollTo(),closeSoftKeyboard()).check(matches(withText(ap1DateString+" - Dr. (pending)")));
-        onData(anything()).inAdapterView(withId(R.id.ptPersonalAppointmentsListView)).atPosition(1).onChildView(withId(R.id.textViewAppointmentTime)).perform(scrollTo(),closeSoftKeyboard()).check(matches(withText(apTimeString2+", 0 minutes")));
-        onData(anything()).inAdapterView(withId(R.id.ptPersonalAppointmentsListView)).atPosition(1).onChildView(withId(R.id.textViewAppointmentLocation)).perform(scrollTo(),closeSoftKeyboard()).check(matches(withText("")));
+        onView(withId(R.id.textViewAppointmentDoctorDate)).check(doesNotExist());
+        onView(withId(R.id.textViewAppointmentTime)).check(doesNotExist());
+        onView(withId(R.id.textViewAppointmentLocation)).check(doesNotExist());
+        onView(withId(R.id.ptNoAppointements)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
     @Test
